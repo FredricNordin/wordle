af6efc178de59537eb1076cc2a171e159fd53b67
FredricNordin <90624100+FredricNordin@users.noreply.github.com>
2022-03-17 10:23:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# wordle
+ Assignment for Lernia
---
+++ index.js
@@ -0,0 +1,14 @@
+algoritmA();
+
+function algoritmA () {
+    const guess = "hello".toUpperCase();
+    const answer = "jumbo".toUpperCase();
+
+    for (let i = 0; i < guess.length; i++) {
+        if (guess[i] == answer[i]) {
+            console.log("Letter" + guess[i] + " is: Correct");
+        } else {
+            console.log("Letter" + guess[i] + " is: Incorrect");
+        }
+    };
+};
