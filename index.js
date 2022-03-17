f1b1b64a7c58b4d55f9cd8aa7b3d724f9b1a6fa8
FredricNordin <90624100+FredricNordin@users.noreply.github.com>
2022-03-17 11:21:05 +0100

    Check if misplaced

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 algoritmA();
 
 function algoritmA () {
-    const guess = "hello".toUpperCase();
+    const guess = "Heljo".toUpperCase();
     const answer = "jumbo".toUpperCase();
 
+    console.log("Your guess is: " + guess);
     for (let i = 0; i < guess.length; i++) {
         if (guess[i] == answer[i]) {
             console.log("Letter " + guess[i] + " is: Correct");
+        } else if (guess[i] !== answer[i] && answer.includes(guess[i])){
+            console.log("Letter " + guess[i] + " is: Misplaced");
         } else {
             console.log("Letter " + guess[i] + " is: Incorrect");
         }
