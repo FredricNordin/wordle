c9ce8043028db2ed5d36085c767c15e18044f48f
FredricNordin <90624100+FredricNordin@users.noreply.github.com>
2022-03-18 19:08:28 +0100

    Correct, misplaced, incorrect fix

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-algoritmA();
+algorithmA();
 
-function algoritmA () {
-    const guess = "hojlo".toUpperCase();
-    const answer = "jumbo".toUpperCase();
-    const output = [];
+function algorithmA() {
+  const guess = "hallå".toUpperCase();
+  const answer = "cykla".toUpperCase();
+  const output = [];
 
+  // For reference to whether the correct letters are in place.
+  const correctCheck = { C: "correct" };
 
-    console.log("Your guess is: " + guess);
-    for (let i = 0; i < guess.length; i++) {
-        if (guess[i] == answer[i]) {
-            console.log("Letter " + guess[i] + " is: Correct");
-            output.push({letter: guess[i], result: "correct"});
-        } else if (guess[i] !== answer[i] && answer.includes(guess[i])){
-            console.log("Letter " + guess[i] + " is: Misplaced");
-            output.push({letter: guess[i], result: "misplaced"});
-        } else {
-            console.log("Letter " + guess[i] + " is: Incorrect");
-            output.push({letter: guess[i], result: "incorrect"});
-        }
-    };
-    console.log(output);
-};
+  // Check which letters are correct.
+  for (let i = 0; i < guess.length; i++) {
+    if (guess[i] === answer[i]) {
+      correctCheck[guess[i]] = "correct";
+    }
+  }
+  // Save each corresponding letter to an array showing which are correct, misplaced and incorrect.
+  for (let i = 0; i < guess.length; i++) {
+    if (guess[i] === answer[i]) {
+      output.push({ letter: guess[i], result: "correct" });
+    } else if (
+      correctCheck[guess[i]] !== "correct" &&
+      answer.includes(guess[i])
+    ) {
+      output.push({ letter: guess[i], result: "misplaced" });
+    } else {
+      output.push({ letter: guess[i], result: "incorrect" });
+    }
+  }
+  // Output the results.
+  console.log(output);
+}
