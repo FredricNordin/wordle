fb772495eb13eb8c915ef00d691b6ad9c326d73a
FredricNordin <90624100+FredricNordin@users.noreply.github.com>
2022-03-18 22:56:14 +0100

    tests

---
+++ test/algorithmA.spec.js
@@ -0,0 +1,77 @@
+import { test, expect } from "@jest/globals";
+import algorithmA from "../src/index.js";
+
+describe("Tests for the wordle algorithm A assignment", () => {
+  test("one letter 'l' should not be misplaced if it has one correct", () => {
+    const output = algorithmA("HALLÅ", "CYKLA");
+
+    expect(output[0].result).toEqual("incorrect");
+    expect(output[1].result).toEqual("misplaced");
+    expect(output[2].result).toEqual("incorrect");
+    expect(output[3].result).toEqual("correct");
+    expect(output[4].result).toEqual("incorrect");
+  });
+
+  test("a + a should be correct", () => {
+    const output = algorithmA("a", "a");
+
+    expect(output[0].result).toEqual("correct");
+  });
+
+  test("a + b should be incorrect", () => {
+    const output = algorithmA("a", "b");
+
+    expect(output[0].result).toEqual("incorrect");
+  });
+
+  test("a + A should be correct", () => {
+    const output = algorithmA("a", "A");
+
+    expect(output[0].result).toEqual("correct");
+  });
+
+  test("aB + AB should be correct", () => {
+    const output = algorithmA("ab", "AB");
+
+    expect(output[0].result).toEqual("correct");
+    expect(output[1].result).toEqual("correct");
+  });
+
+  test("bA + Ab should be correct", () => {
+    const output = algorithmA("ab", "AB");
+
+    expect(output[0].result).toEqual("correct");
+    expect(output[1].result).toEqual("correct");
+  });
+
+  test("check if all is correct", () => {
+    const output = algorithmA("llooll", "llooll");
+
+    expect(output[0].result).toEqual("correct");
+    expect(output[1].result).toEqual("correct");
+    expect(output[2].result).toEqual("correct");
+    expect(output[3].result).toEqual("correct");
+    expect(output[4].result).toEqual("correct");
+    expect(output[5].result).toEqual("correct");
+  });
+
+  test("check if all is incorrect", () => {
+    const output = algorithmA("llooll", "knarka");
+
+    expect(output[0].result).toEqual("incorrect");
+    expect(output[1].result).toEqual("incorrect");
+    expect(output[2].result).toEqual("incorrect");
+    expect(output[3].result).toEqual("incorrect");
+    expect(output[4].result).toEqual("incorrect");
+    expect(output[5].result).toEqual("incorrect");
+  });
+
+  test("check if all is misplaced", () => {
+    const output = algorithmA("akms", "mask");
+
+    expect(output[0].result).toEqual("misplaced");
+    expect(output[1].result).toEqual("misplaced");
+    expect(output[2].result).toEqual("misplaced");
+    expect(output[3].result).toEqual("misplaced");
+  });
+});
