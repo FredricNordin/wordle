5ea57c2869c836995a49dbe7c5bd954547b0d171
FredricNordin <90624100+FredricNordin@users.noreply.github.com>
2022-03-18 23:05:55 +0100

    better strategy

--- test/algorithmA.spec.js
+++ test/algorithmA.spec.js
@@ -12,39 +12,39 @@ describe("Tests for the wordle algorithm A assignment", () => {
     expect(output[4].result).toEqual("incorrect");
   });
 
-  test("a + a should be correct", () => {
+  test("a + a should be correct because of two same letters", () => {
     const output = algorithmA("a", "a");
 
     expect(output[0].result).toEqual("correct");
   });
 
-  test("a + b should be incorrect", () => {
+  test("a + b should be incorrect because of two different letters", () => {
     const output = algorithmA("a", "b");
 
     expect(output[0].result).toEqual("incorrect");
   });
 
-  test("a + A should be correct", () => {
+  test("a + A should be correct small and big letters match", () => {
     const output = algorithmA("a", "A");
 
     expect(output[0].result).toEqual("correct");
   });
 
-  test("aB + AB should be correct", () => {
+  test("aB + AB should be correct/correct small guess letters and big answer letters match", () => {
     const output = algorithmA("ab", "AB");
 
     expect(output[0].result).toEqual("correct");
     expect(output[1].result).toEqual("correct");
   });
 
-  test("bA + Ab should be misplaced/misplaced", () => {
+  test("bA + Ab should be misplaced/misplaced mixed small and big letters but misplaced", () => {
     const output = algorithmA("bA", "Ab");
 
     expect(output[0].result).toEqual("misplaced");
     expect(output[1].result).toEqual("misplaced");
   });
 
-  test("check if all is correct", () => {
+  test("test if all can be correct", () => {
     const output = algorithmA("llooll", "llooll");
 
     expect(output[0].result).toEqual("correct");
@@ -55,7 +55,7 @@ describe("Tests for the wordle algorithm A assignment", () => {
     expect(output[5].result).toEqual("correct");
   });
 
-  test("check if all is incorrect", () => {
+  test("test if all can be incorrect", () => {
     const output = algorithmA("llooll", "knarka");
 
     expect(output[0].result).toEqual("incorrect");
@@ -66,7 +66,7 @@ describe("Tests for the wordle algorithm A assignment", () => {
     expect(output[5].result).toEqual("incorrect");
   });
 
-  test("check if all is misplaced", () => {
+  test("test if all can be misplaced", () => {
     const output = algorithmA("akms", "mask");
 
     expect(output[0].result).toEqual("misplaced");
